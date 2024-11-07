c486b963d73dc943a227ecc6b2e940fa1025c0e3
AlenaHud <Alenaa.hud@yandex.ru>
2024-11-07 13:07:45 +0300

    добавила timedelta и плейлисты

--- main.py
+++ main.py
@@ -63,3 +63,30 @@ def get_duration(playlist: iter, n: int):
             sec = float(currTm[-2:])
         else:
             sec = float(currTm[-1])
+
+        currTm = timedelta(minutes=min, seconds=sec) # Формат мин.сек
+        sumTm += currTm # Ввели новую переменную
+
+    return sumTm
+
+
+playlist_d = [
+    ("The Flute Tune", "Voodoo People", "Galvanize", "Miami Disco", "Komarovo", "Wild Frontier", "Check It Out", "Seasons", "These Things Will Come To Be"),
+    (5.23, 5.07, 7.34, 4.31, 2.26, 4.28, 2.09, 4.25, 4.56),
+]
+
+playlist_b = {
+    'Портофино': 3.32,
+    'Снег': 4.35,
+    'Попытка №5': 3.23,
+    'Тополиный Пух': 3.53,
+    'Если хочешь остаться': 4.48,
+    'Зеленоглазое такси': 5.52,
+    'Ты не верь слезам': 3.1,
+    'Nowhere to Run': 2.58,
+    'Салют, Вера': 4.41,
+    'Улетаю': 3.24,
+    'Опять метель': 3.37,
+    }
+
+#остаётся только сделать input для введения песен и print результата
